fbd80db5556feac0de6bb2e3dd2fc9a5b12ee317
MaximSidorov1996 <131602849+MaximSidorov1996@users.noreply.github.com>
2023-09-23 09:48:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+with open("index.html", "r") as f:
+    page_content = f.read()
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
